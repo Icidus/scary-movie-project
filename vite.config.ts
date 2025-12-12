bab4c75f429c7aad2fd596d3f5594949180c7d16
Rob O'Connell <roconnell@smith.edu>
2025-12-11 19:54:07 -0500

    Add Github Pages workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
+  base: mode === "production" ? "/scary-movie-project/" : "/", // "/" in dev, subpath in prod
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+}));
